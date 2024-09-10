fe9791f49a8d94e6dc5f03bb303eeca0ce97e244
Júlia Lima <julia.lima@neki-it.com.br>
2024-09-10 18:12:47 -0300

    feat: json.server

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,30 @@
+import { useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import Header from "components/Header";
 import styles from "./Home.module.scss";
 import relogio from "assets/inicial.png";
 import Button from "components/Button";
+import instance from "common/config/api";
+import { adicionarCategorias } from "store/reducers/categorias";
 
 export default function Home() {
   const navigate = useNavigate();
 
+  const dispatch = useDispatch();
+
   const categorias = useSelector((state) => state.categorias);
+
+  const buscarCategorias = useCallback(async () => {
+    const response = await instance.get("/categorias");
+
+    dispatch(adicionarCategorias(response.data));
+  }, [dispatch]);
+
+  useEffect(() => {
+    buscarCategorias();
+  }, [buscarCategorias]);
 
   return (
     <div>
